4e3be814e9f66702ba49ae1eeb3027ad89c59df5
Luiz Almada <luizalmada@yahoo.com>
2025-09-20 01:53:57 -0300

    refact: config app.listen of render

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import { env } from './env'
 
 app
   .listen({
+    // Config para o Render
+    host: '0.0.0.0', // IMPORTANTE: escutar em 0.0.0.0, não localhost
     port: env.PORT,
   })
   .then(() => {
